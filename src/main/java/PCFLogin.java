ec627e7495a4a8a56ff08ae5f74fe78b13b01a09
Krutika Kamdi <kk00128898@techmahindra.com>
2019-03-17 15:44:12 -0700

    no message

---
+++ src/main/java/PCFLogin.java
@@ -0,0 +1,43 @@
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class PCFLogin {
+
+	private static WebDriver driver;
+
+	@Test
+	public void loginPCF() {
+		login();
+	}
+
+	private static boolean login(){
+
+		String sUsername = "ranimonal@yahoo.co.in";
+		String sPassword = "Khushi@1234M";
+		String url = "https://console.run.pivotal.io/";
+		
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--safebrowsing-disable-download-protection");
+		System.setProperty("webdriver.chrome.driver", "/Users/krutikakamdi/Downloads/chromedriver");
+		driver = new ChromeDriver(options);
+		driver.get(url);
+
+		WebElement txtUsername = driver.findElement(By.id("email"));
+		WebElement btnNext = driver.findElement(By.name("commit"));
+		WebElement txtPassword = driver.findElement(By.name("password"));
+	//	WebElement btnSubmit = driver.findElement(By.cssSelector(".island-button"));
+		
+		txtUsername.sendKeys(sUsername);
+		btnNext.click();
+		txtPassword.sendKeys(sPassword);
+		btnNext.click();
+		driver.manage().window().maximize();
+		return true;
+	}
+	
+}
---
+++ src/test/java/com/test/PCFLogin.java
@@ -0,0 +1,75 @@
+package com.test;
+import java.text.SimpleDateFormat;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+/*import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+*/
+public class PCFLogin{
+private static WebDriver driver;
+/*String sTcName = this.getClass().getSimpleName();
+public static ExtentReports extent;
+public static ExtentTest test;
+public static ExtentTest logger;
+public static ITestResult result;
+@BeforeSuite
+public void beforesuite() {
+	String timestamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new java.util.Date());
+	
+	extent = new ExtentReports(System.getProperty("user.dir")+"\\Reporting\\"+sTcName+" "+timestamp+".html");
+}
+
+@BeforeMethod
+public void beforemethod() {
+	test = extent.startTest(this.getClass().getSimpleName(), "testcase");
+	test.assignAuthor("Krutika");
+	test.assignCategory("SOA-Report");
+}*/
+
+@BeforeTest
+public void baseurl() {
+	String url = "https://console.run.pivotal.io/";
+	System.setProperty("webdriver.chrome.driver", "/Users/krutikakamdi/Downloads/chromedriver");
+	driver = new ChromeDriver();
+	driver.get(url);
+}
+	@Test
+	public void loginPCF() throws InterruptedException {
+
+
+		String sUsername = "ranimonal@yahoo.co.in";
+		String sPassword = "Khushi@1234M";
+		//Thread.sleep(10000);
+		WebElement txtUsername = driver.findElement(By.xpath("//input[@name='email']"));
+		txtUsername.sendKeys(sUsername);
+		WebElement btnNext = driver.findElement(By.xpath("//div/input[@name='commit']"));
+		
+		btnNext.click();
+		WebElement txtPassword = driver.findElement(By.name("password"));
+		
+		txtPassword.sendKeys(sPassword);
+		btnNext = driver.findElement(By.xpath("//div/input[@value='Sign in']"));
+		btnNext.click();
+		driver.manage().window().maximize();
+
+	}
+	/*@AfterTest
+	public void endSession() {
+		driver.quit();
+	
+	}*/
+	
+	
+}
